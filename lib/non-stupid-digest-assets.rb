dfd171846e10b98f2b85dc4610b2dc47486e55e5
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2013-12-24 11:14:30 +0200

    Update non-stupid-digest-assets.rb

--- lib/non-stupid-digest-assets.rb
+++ lib/non-stupid-digest-assets.rb
@@ -1,18 +1,38 @@
+module NonStupidDigestAssets
+  mattr_accessor :whitelist
+  @@whitelist = []
+end
+
 module Sprockets
   class Manifest
     def compile_with_non_digest *args
       compile_without_non_digest *args
-
-      files.each do |(digest_path, info)|
+      if NonStupidDigestAssets.whitelist.empty?
+        files_to_copy = files
+      else
+        files_to_copy = files.select do |file, info|
+          !NonStupidDigestAssets.whitelist.detect do |item|
+            info['logical_path'] =~ /#{item}/
+          end.nil?
+        end
+      end
+      files_to_copy.each do |(digest_path, info)|
         full_digest_path = File.join dir, digest_path
         full_digest_gz_path = "#{full_digest_path}.gz"
         full_non_digest_path = File.join dir, info['logical_path']
         full_non_digest_gz_path = "#{full_non_digest_path}.gz"
-        logger.info "Writing #{full_non_digest_path}"
-        FileUtils.cp full_digest_path, full_non_digest_path
+
+        if File.exists? full_digest_path
+          logger.info "Writing #{full_non_digest_path}"
+          FileUtils.cp full_digest_path, full_non_digest_path
+        else
+          logger.warn "Could not find: #{full_digest_path}"
+        end
         if File.exists? full_digest_gz_path
           logger.info "Writing #{full_non_digest_gz_path}"
           FileUtils.cp full_digest_gz_path, full_non_digest_gz_path
+        else
+          logger.warn "Could not find: #{full_digest_gz_path}"
         end
       end
     end
