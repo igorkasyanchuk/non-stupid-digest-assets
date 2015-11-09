e6edb3ba284dd480bfe85b8b7fe01a32079b7bd7
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2015-11-09 06:23:07 -0500

    fix for undefined method

--- lib/non-stupid-digest-assets.rb
+++ lib/non-stupid-digest-assets.rb
@@ -3,8 +3,56 @@ module NonStupidDigestAssets
   @@whitelist = []
 end
 
+module OldSprockets
+  def compile(*args)
+    unless environment
+      raise Error, "manifest requires environment for compilation"
+    end
+
+    paths = environment.each_logical_path(*args).to_a +
+      args.flatten.select { |fn| Pathname.new(fn).absolute? if fn.is_a?(String)}
+
+    paths.each do |path|
+      if asset = find_asset(path)
+        files[asset.digest_path] = {
+          'logical_path' => asset.logical_path,
+          'mtime'        => asset.mtime.iso8601,
+          'size'         => asset.bytesize,
+          'digest'       => asset.digest
+        }
+        logical_path = asset.logical_path
+        assets[asset.logical_path] = asset.digest_path
+
+        target = File.join(dir, asset.digest_path)
+
+        if File.exist?(target)
+          logger.debug "Skipping #{target}, already exists"
+        else
+          logger.info "Writing #{target}"
+          asset.write_to target
+          asset.write_to "#{target}.gz" if asset.is_a?(BundledAsset)
+        end
+
+        if ENV['SPROCKETS_NON_DIGEST']
+          logical_target = File.join(dir, logical_path)
+          logger.info "Writing #{logical_target}"
+          asset.write_to logical_target
+          asset.write_to "#{logical_target}.gz" if asset.is_a?(BundledAsset)
+        end
+
+        save
+        asset
+      end
+    end
+  end
+end
+
 module Sprockets
   class Manifest
+    unless respond_to?(:compile)
+      include OldSprockets
+    end
+
     def compile_with_non_digest *args
       compile_without_non_digest *args
       if NonStupidDigestAssets.whitelist.empty?
